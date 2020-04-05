7ae5926b31677a04c58399a1e43ccfd6ca586940
Tilkeii <florian@gouarin.info>
2020-04-05 20:28:50 +0200

    build(environment variables): add environment variable system to the project
    Add three environment files to the project and modify app module. I added also commitizen to the
    project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,46 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { CommonModule } from './common/common.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import Joi = require('@hapi/joi');
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [
+        './src/environments/.env',
+        process.env.NODE_ENV === 'production'
+          ? './src/environments/.env.production'
+          : './src/environments/.env.development',
+      ],
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        DB_HOST: Joi.string().default('localhost'),
+        DB_PORT: Joi.number().default(5432),
+        DB_USERNAME: Joi.string().default('postgres'),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().default('leafer'),
+      }),
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [],
+      synchronize: true,
+    }),
+    CommonModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
