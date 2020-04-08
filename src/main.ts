5013428bad1bff5e4a652b468d6de696464ef6c3
Tilkeii <florian@gouarin.info>
2020-04-08 18:25:14 +0200

    chore(main): update travis with a default command & update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT || 3000);
+  const logger = new Logger("Main");
+  const port = process.env.PORT || 3000
+  logger.log(`Application started in ${process.env.NODE_ENV} with the port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
