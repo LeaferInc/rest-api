5a7e21b32f3a78cb7d4641985c8f455ea4fe8202
Tilkeii <florian@gouarin.info>
2020-04-08 19:01:26 +0200

    add user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
 import { CommonModule } from './common/common.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
@@ -38,6 +39,7 @@ import Joi = require('@hapi/joi');
       synchronize: true,
     }),
     CommonModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
